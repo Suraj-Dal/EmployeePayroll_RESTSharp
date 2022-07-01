2ce0cdcd1b23bbaeb87f2e96d17c836cc3a88f10
Suraj-Dal <dsurajdal10@gmail.com>
2022-07-01 12:59:45 +0530

    [Suraj]: Usecase 3 to add multiple employees to JSON Server is added.

--- RestAPITest/RestAPITest/RestSharpTest.cs
+++ RestAPITest/RestAPITest/RestSharpTest.cs
@@ -16,7 +16,7 @@ namespace RestAPITest
             RestResponse response = restClient.Execute(request);
             Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
             List<Employee> list = JsonConvert.DeserializeObject<List<Employee>>(response.Content);
-            Assert.AreEqual(4, list.Count);
+            Assert.AreEqual(9, list.Count);
             foreach (Employee e in list)
             {
                 Console.WriteLine("ID: " + e.Id + "\nName: " + e.Name + "\nSalary: " + e.Salary);
@@ -35,6 +35,25 @@ namespace RestAPITest
             Assert.AreEqual("Ravi", data.Name);
             Assert.AreEqual("25000", data.Salary);
             Console.WriteLine(response.Content);
+        }
+        [Test]
+        public void addMultipleEmpData_ShouldAddtoJSONServer()
+        {
+            restClient = new RestClient("http://localhost:4000");
+            List<Employee> list = new List<Employee>();
+            list.Add(new Employee { Id = 8, Name = "Om", Salary = "21000" });
+            list.Add(new Employee { Id = 9, Name = "Mohan", Salary = "22000" });
+            list.ForEach(body =>
+            {
+                RestRequest request = new RestRequest("/employees", Method.Post);
+                request.AddParameter("application/json", body, ParameterType.RequestBody);
+                RestResponse response = restClient.Execute(request);
+                Assert.AreEqual(response.StatusCode, HttpStatusCode.Created);
+                Employee data = JsonConvert.DeserializeObject<Employee>(response.Content);
+                Assert.AreEqual(body.Name, data.Name);
+                Assert.AreEqual(body.Salary, data.Salary);
+                Console.WriteLine(response.Content);
+            });
         }
     }
 }
